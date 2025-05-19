55c6a874e2c91adef3f523d7b6bf87ba056d9d18
moisterr <animeotaku081@gmail.com>
2025-05-19 17:42:13 +0700

    no uart between stm and esp

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,7 +2,6 @@
 #include <GPIO_INIT.h>
 #include <MONITOR_CONTROL.h>
 #include <TIM2_PWM.h>
-#include <USART1.h>
 #include "system_clock.h"
 
 int main(void)
@@ -10,15 +9,11 @@ int main(void)
     SystemClock_Config();
     GPIO_Init();
     ADC_Init();
-    USART1_Config();
     TIM2_Init(1000, 999);
 
     while (1)
     {
         uint16_t adc_value = ADC_Read();
-        USART1_SendString("ADC = ");
-        USART1_SendNumber(adc_value);
-        USART1_SendString("\r\n");
         Motor_Control(adc_value);
     }
 }
--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -1,9 +1,8 @@
 Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/ADC_INIT.h \
  ../Core/Inc/GPIO_INIT.h ../Core/Inc/MONITOR_CONTROL.h \
- ../Core/Inc/TIM2_PWM.h ../Core/Inc/USART1.h ../Core/Inc/system_clock.h
+ ../Core/Inc/TIM2_PWM.h ../Core/Inc/system_clock.h
 ../Core/Inc/ADC_INIT.h:
 ../Core/Inc/GPIO_INIT.h:
 ../Core/Inc/MONITOR_CONTROL.h:
 ../Core/Inc/TIM2_PWM.h:
-../Core/Inc/USART1.h:
 ../Core/Inc/system_clock.h:
