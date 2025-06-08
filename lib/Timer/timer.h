35d48e86cc8431bb1af53d177722c165c77d0219
phan-pham-584 <phanpham2k4@gmail.com>
2025-06-08 13:39:36 +0700

    Update timer.h

--- lib/Timer/timer.h
+++ lib/Timer/timer.h
@@ -20,6 +20,6 @@ void Timer_Init(int group, int timer, uint16_t prescaler);
  * @param timer Timer number (0 or 1)
  * @param ms Delay time in milliseconds
  */
-void Timer_Delay(int group, int timer, uint32_t ms);
+void Timer_Delay(int group, int timer, uint64_t ms);
 
 #endif // TIMER_H
